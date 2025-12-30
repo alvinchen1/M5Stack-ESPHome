d435fc1e0f982c717fcd6d9d1d1abd7ceb9ebec4
Kevin Lewis <lewi3069@gmail.com>
2025-12-29 22:48:25 -0500

    Simplify camera.py for debugging

--- components/camera_mlx90640/camera.py
+++ components/camera_mlx90640/camera.py
@@ -1,26 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import camera
-from esphome.const import CONF_ID
-from . import MLX90640, mlx90640_ns
 
-
-
-CONF_MLX90640_ID = "camera_mlx90640_id"
-
-MLX90640Camera = mlx90640_ns.class_("MLX90640Camera", camera.Camera)
-
-CONFIG_SCHEMA = camera.CAMERA_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(MLX90640Camera),
-        cv.GenerateID(CONF_MLX90640_ID): cv.use_id(MLX90640),
-    }
-)
-
+CONFIG_SCHEMA = camera.CAMERA_SCHEMA.extend({})
 PLATFORM_SCHEMA = CONFIG_SCHEMA
 
 async def to_code(config):
-    hub = await cg.get_variable(config[CONF_MLX90640_ID])
-    var = cg.new_Pvariable(config[CONF_ID], hub)
-    await cg.register_component(var, config)
-    await camera.register_camera(var, config)
+    pass
