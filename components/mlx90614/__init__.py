fc039b374985da399138553c87343296a6b6a263
alvinchen1 <26409535+alvinchen1@users.noreply.github.com>
2026-01-01 16:14:17 -0500

    update to init py

--- components/mlx90614/__init__.py
+++ components/mlx90614/__init__.py
@@ -4,12 +4,13 @@ from esphome.const import (
     CONF_ID,
     CONF_ADDRESS,
     CONF_UPDATE_INTERVAL,
-    CONF_AMBIENT,
-    CONF_OBJECT,
 )
 
 from esphome.components import i2c, sensor
 
+# Custom YAML keys
+CONF_AMBIENT_TEMPERATURE = "ambient_temperature"
+CONF_OBJECT_TEMPERATURE = "object_temperature"
 CONF_DIAGNOSTIC_MODE = "diagnostic_mode"
 
 mlx90614_ns = cg.esphome_ns.namespace("mlx90614")
@@ -24,13 +25,15 @@ CONFIG_SCHEMA = (
             cv.Required(CONF_ADDRESS): cv.i2c_address,
             cv.Optional(CONF_UPDATE_INTERVAL, default="5s"): cv.update_interval,
             cv.Optional(CONF_DIAGNOSTIC_MODE, default=False): cv.boolean,
-            cv.Optional(CONF_AMBIENT): sensor.sensor_schema(
+
+            cv.Optional(CONF_AMBIENT_TEMPERATURE): sensor.sensor_schema(
                 unit_of_measurement="°C",
                 accuracy_decimals=1,
                 device_class="temperature",
                 state_class="measurement",
             ),
-            cv.Optional(CONF_OBJECT): sensor.sensor_schema(
+
+            cv.Optional(CONF_OBJECT_TEMPERATURE): sensor.sensor_schema(
                 unit_of_measurement="°C",
                 accuracy_decimals=1,
                 device_class="temperature",
@@ -52,10 +55,10 @@ async def to_code(config):
     cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
     cg.add(var.set_diagnostic_mode(config[CONF_DIAGNOSTIC_MODE]))
 
-    if CONF_AMBIENT in config:
-        ambient = await sensor.new_sensor(config[CONF_AMBIENT])
+    if CONF_AMBIENT_TEMPERATURE in config:
+        ambient = await sensor.new_sensor(config[CONF_AMBIENT_TEMPERATURE])
         cg.add(var.set_ambient_sensor(ambient))
 
-    if CONF_OBJECT in config:
-        obj = await sensor.new_sensor(config[CONF_OBJECT])
+    if CONF_OBJECT_TEMPERATURE in config:
+        obj = await sensor.new_sensor(config[CONF_OBJECT_TEMPERATURE])
         cg.add(var.set_object_sensor(obj))
